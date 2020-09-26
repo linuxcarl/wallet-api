5adb1552608e2bcc7062f82b4c3844171c5dca21
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-26 10:47:01 -0500

    added clontroller check by run routes check and date

---
+++ src/controllers/check.controller.ts
@@ -0,0 +1,21 @@
+import { GET, route } from 'awilix-express';
+import { Request, Response } from 'express';
+import { DateService } from '../services/date.service';
+@route('/check')
+export class CheckController {
+  public constructor(private readonly dateService: DateService) {}
+
+  @GET()
+  public index(req: Request, res: Response): void {
+    res.send({
+      controller: 'default controller',
+    });
+  }
+  @route('date')
+  @GET()
+  public date(req: Request, res: Response): void {
+    res.send({
+      date: this.dateService.get(),
+    });
+  }
+}
