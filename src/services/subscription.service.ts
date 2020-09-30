d8b3973a104c33219e682fea8dedd0cd0698e392
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-29 21:35:05 -0500

    completed method store by subscription service

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -1,4 +1,5 @@
 import { promises } from 'fs';
+import { ApplicationException } from '../common/exceptions/application.exception';
 import { SubscriptionCreateDto } from '../dtos/subscription.dto';
 import { ISubscriptionReposirtory } from './repositories/domain/imp/subscription.respository.interface';
 import { Subscription } from './repositories/domain/subscription';
@@ -19,6 +20,15 @@ export class SubscriptionService {
       user_id,
       code
     );
+    if (!originalEntry) {
+      try {
+        await this.subscriptionRepository.store(entry as Subscription);
+      } catch (error) {
+        throw new ApplicationException(error.message);
+      }
+    } else {
+      throw new ApplicationException('User subscription already exists.');
+    }
   }
   public async update(entry: any): Promise<void> {
     this.subscriptionRepository.update(entry);
