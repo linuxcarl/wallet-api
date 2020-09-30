3d0fb19ef8113e4e4626cf0f9288ba23eedd37c3
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-29 19:56:03 -0500

    -implements subscriptionRepository in the container by injection

--- src/container.ts
+++ src/container.ts
@@ -2,6 +2,7 @@ import { asClass, createContainer } from 'awilix';
 import { DateService } from './services/date.service';
 import express from 'express';
 import { scopePerRequest } from 'awilix-express';
+import { SubscriptionMysqlRepository } from './services/repositories/domain/imp/mysql/subscription.repository';
 
 export default (app: express.Application) => {
   const container = createContainer({
@@ -9,6 +10,7 @@ export default (app: express.Application) => {
   });
 
   container.register({
+    subscriptionRepository: asClass(SubscriptionMysqlRepository).scoped(),
     dateService: asClass(DateService).scoped(),
   });
 
