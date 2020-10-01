db532d40214426e0deea5c73cc03e64024378b64
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-30 19:05:03 -0500

    added respository and service in the container by injection

--- src/container.ts
+++ src/container.ts
@@ -4,6 +4,9 @@ import express from 'express';
 import { scopePerRequest } from 'awilix-express';
 import { SubscriptionMysqlRepository } from './services/repositories/domain/imp/mysql/subscription.repository';
 import { SubscriptionService } from './services/subscription.service';
+import { MovementMysqlRepository } from './services/repositories/domain/imp/mysql/movement.respository';
+import { BalanceMysqlRepository } from './services/repositories/domain/imp/mysql/balance.respository';
+import { MovementService } from './services/movement.service';
 
 export default (app: express.Application) => {
   const container = createContainer({
@@ -12,8 +15,11 @@ export default (app: express.Application) => {
 
   container.register({
     subscriptionRepository: asClass(SubscriptionMysqlRepository).scoped(),
+    movementRepository: asClass(MovementMysqlRepository).scoped(),
+    balanceRepository: asClass(BalanceMysqlRepository).scoped(),
 
     subscriptionService: asClass(SubscriptionService).scoped(),
+    movementService: asClass(MovementService).scoped(),
     dateService: asClass(DateService).scoped(),
   });
 
