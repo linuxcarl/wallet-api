52b800a95fbd07ca7ae560047ff1578ac5ec69a1
Carlos Ramirez <carlos@extrasistemas.com>
2020-10-01 19:08:17 -0500

    added unit test by method POST in movemnts

--- src/services/movement.service.spec.ts
+++ src/services/movement.service.spec.ts
@@ -2,12 +2,13 @@ import { MovementService } from './movement.service';
 import { BalanceMockRepository } from './repositories/imp/mocks/balance.repository';
 import { MovementMockRepository } from './repositories/imp/mocks/movement.repository';
 import db from '../common/persistence/mock.persistence';
+import { ApplicationException } from '../common/exceptions/application.exception';
 const movementService = new MovementService(
   new MovementMockRepository(),
   new BalanceMockRepository()
 );
 
-describe('Test by movement services', () => {
+describe('GET - Test by movement services', () => {
   it('Should be to have object with all records - method all()', async () => {
     const expected = db.movements;
     const result = await movementService.all();
@@ -26,5 +27,59 @@ describe('Test by movement services', () => {
     const result = await movementService.find(1);
 
     expect(result).toMatchObject(expected);
+  });
+});
+describe('POST - Test by movement services', () => {
+  it('Should be to return inserted id INCOME - method store()', async () => {
+    const expected = 0;
+    const data = {
+      user_id: 2,
+      amount: 500,
+      type: 0,
+    };
+    const result = await movementService.store(data);
+
+    expect(result).toBeGreaterThan(expected);
+  });
+
+  it('Should be to return inserted id OUTCOME - method store()', async () => {
+    const expected = 0;
+    const data = {
+      user_id: 2,
+      amount: 500,
+      type: 1,
+    };
+    const result = await movementService.store(data);
+
+    expect(result).toBeGreaterThan(expected);
+  });
+
+  it('Should be to return throw error OUTCOME- method store()', async () => {
+    const expected = 'User does not have enough balance';
+    const data = {
+      user_id: 2,
+      amount: 22500,
+      type: 1,
+    };
+
+    try {
+      await movementService.store(data);
+    } catch (error) {
+      expect(error.message).toBe(expected);
+    }
+  });
+
+  it('Should be to return throw error Invalid - method store()', async () => {
+    const expected = 'Invalid movement type supplied';
+    const data = {
+      user_id: 2,
+      amount: 22500,
+      type: 7,
+    };
+    try {
+      const result = await movementService.store(data);
+    } catch (error) {
+      expect(error.message).toBe(expected);
+    }
   });
 });
